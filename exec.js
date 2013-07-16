843c5fa8f2247025c75dd23ad615adfe79d8b58b
simofuji <simofuji@paperboy.co.jp>
2013-07-16 18:13:00 +0900

    * フィルタを追加 * ソースが乱れた...orz * フィルタ名を表示した

--- exec.js
+++ exec.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	grayscale = function() {
+	execute = function() {
 		//元の画像が読み込まれたら、その下に画像を表示する
 		var img = document.getElementById('orig');
 		//origの画像にイベントを追加
@@ -34,8 +34,40 @@ $(document).ready(function(){
 				c.style.display = 'inline';
 			}
 		}
+
 		runFilter('grayscale', Filters.grayscale);
 		runFilter('brightness', Filters.brightness, 40);
 		runFilter('threshold', Filters.threshold, 128);
+        runFilter('sharpen', Filters.convolute, [  0, -1,  0, 
+                                                   -1,  5, -1, 
+                                                   0, -1,  0 ] );
+        runFilter('blur', Filters.convolute, [  1/9, 1/9, 1/9, 
+                                                1/9, 1/9, 1/9, 
+                                                1/9, 1/9, 1/9] );
+ 
+
+        runFilter('sobel', function(px) {
+              px = Filters.grayscale(px);
+              var vertical = Filters.convoluteFloat32(px,
+                 [-1,-2,-1,
+                 0, 0, 0,
+                 1, 2, 1]);
+              var horizontal = Filters.convoluteFloat32(px,
+                 [-1,0,1,
+                 -2,0,2,
+                 -1,0,1]);
+              var id = Filters.createImageData(vertical.width, vertical.height);
+              for (var i=0; i<id.data.length; i+=4) {
+              var v = Math.abs(vertical.data[i]);
+              id.data[i] = v;
+              var h = Math.abs(horizontal.data[i]);
+              id.data[i+1] = h
+              id.data[i+2] = (v+h)/4;
+              id.data[i+3] = 255;
+              }
+              return id;
+              }); 
+
+
 	}
 });
